4f2f9a43ef90a01558865456b82f9a07abde000c
Jim Campbell <jc@jimcampbell.tv>
2024-08-19 17:27:56 -0700

    moved functions to a .h

---
+++ camera_grab.h
@@ -0,0 +1,18 @@
+
+#ifndef CAMERA_GRAB_H
+#define CAMERA_GRAB_H
+
+#include <opencv2/opencv.hpp>
+
+
+
+// Loads a grayscale image
+cv::Mat loadImage(const std::string &imageFile);
+
+cv::VideoCapture InitWebCam(bool &valid_cam, int Horizontal_Res, int Vertical_Res);
+
+void get_camera_frame(cv::VideoCapture &capture, cv::Mat &gray_frame_local, cv::Mat &diff_frame); 
+
+void get_camera_frame_Test(cv::VideoCapture &capture, cv::Mat &gray_frame_local);
+
+#endif // CAMERA_GRAB_H
