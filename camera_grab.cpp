d8747cdb0cf54353fa0da2882a4bcc6666b4383a
jcamsfo <jc@jimcampbell.tv>
2024-08-25 12:39:09 -0700

    added image write

--- camera_grab.cpp
+++ camera_grab.cpp
@@ -3,9 +3,6 @@
 #include <chrono>
 #include "camera_grab.h"
 
-
-
-
 // Load a grayscale image
 cv::Mat loadImage(const std::string &imageFile)
 {
@@ -17,7 +14,6 @@ cv::Mat loadImage(const std::string &imageFile)
     }
     return img;
 }
-
 
 cv::VideoCapture InitWebCam(bool &valid_cam, int Horizontal_Res, int Vertical_Res) // bool init, cv::Mat gray_frame)
 {
@@ -38,31 +34,32 @@ cv::VideoCapture InitWebCam(bool &valid_cam, int Horizontal_Res, int Vertical_Re
     }
 }
 
-
-bool get_camera_frame(cv::VideoCapture &capture, cv::Mat &gray_frame_local, cv::Mat &diff_frame, 
-                            const float Cycle_Time_In, const int Motion_H_Pos, const int Motion_V_Pos,
-                            const int Noise_Thresh, const int Motion_Thresh)
+bool get_camera_frame(cv::VideoCapture &capture, cv::Mat &gray_frame_local, cv::Mat &diff_frame,
+                      const float Cycle_Time_In, const int Motion_H_Pos, const int Motion_V_Pos,
+                      const int Noise_Thresh, const int Motion_Thresh)
 {
     static int loop_count = 418;
 
     static auto Sample_Time_Stored = std::chrono::steady_clock::now();
-    static auto Sample_Time_Current = std::chrono::steady_clock::now();    
+    static auto Sample_Time_Current = std::chrono::steady_clock::now();
     static std::chrono::duration<double> Cycle_Time = Sample_Time_Current - Sample_Time_Stored;
     static bool full_cycle = false;
 
-    static cv::Mat frame_local; // (768, 1024, CV_8UC1); 
-    static cv::Mat prev_frame; // (768, 1024, CV_8UC1);
+    static cv::Mat frame_local; // (768, 1024, CV_8UC1);
+    static cv::Mat prev_frame;  // (768, 1024, CV_8UC1);
 
     static cv::Mat masked_frame_previous;
     static cv::Mat masked_frame_current;
 
+    static cv::Scalar sum_diff ;
+
     static bool Image_Motion = false;
 
     int diff_frame_width = diff_frame.cols;
-    int diff_frame_height = diff_frame.rows;    
+    int diff_frame_height = diff_frame.rows;
 
-    masked_frame_previous.create(diff_frame.rows, diff_frame.cols, CV_8UC1); 
-    masked_frame_current.create(diff_frame.rows, diff_frame.cols, CV_8UC1); 
+    masked_frame_previous.create(diff_frame.rows, diff_frame.cols, CV_8UC1);
+    masked_frame_current.create(diff_frame.rows, diff_frame.cols, CV_8UC1);
 
     int screen_width = gray_frame_local.cols;
     int screen_height = gray_frame_local.rows;
@@ -71,25 +68,23 @@ bool get_camera_frame(cv::VideoCapture &capture, cv::Mat &gray_frame_local, cv::
 
     cv::Rect roi(Motion_H_Pos, Motion_V_Pos, diff_frame_width, diff_frame_height);
 
-
     loop_count++;
     if (loop_count >= 440)
         loop_count = 0;
 
-    Sample_Time_Current = std::chrono::steady_clock::now();  
+    Sample_Time_Current = std::chrono::steady_clock::now();
     Cycle_Time = Sample_Time_Current - Sample_Time_Stored;
-
 
     Image_Motion = false;
 
     // wait for half the cycle time to capture a frame
-    if(Cycle_Time.count() >= Cycle_Time_In/2 )
+    if (Cycle_Time.count() >= Cycle_Time_In / 2)
     {
         Sample_Time_Stored = Sample_Time_Current;
 
         auto loopStartTime = std::chrono::steady_clock::now();
 
-        capture >> frame_local;                     // // Camera Size Color
+        capture >> frame_local; // // Camera Size Color
         // If the frame is empty, break the loop
         if (frame_local.empty())
         {
@@ -98,17 +93,16 @@ bool get_camera_frame(cv::VideoCapture &capture, cv::Mat &gray_frame_local, cv::
         }
 
         // store previous image for motion detect  (not sent to the screen)
-        if(full_cycle)
+        if (full_cycle)
         {
             // Convert to BW   (Camera Size BW)
-            cv::cvtColor(frame_local, prev_frame, cv::COLOR_BGR2GRAY);      // Convert to BW   Camera Size BW
+            cv::cvtColor(frame_local, prev_frame, cv::COLOR_BGR2GRAY); // Convert to BW   Camera Size BW
 
             // Resize the grayscale camera frame to Screen Size BW
-            cv::resize(prev_frame, prev_frame, cv::Size(screen_width, screen_height));      
+            cv::resize(prev_frame, prev_frame, cv::Size(screen_width, screen_height));
 
             // Create smaller image for detecting motion in the central 75%
-            masked_frame_previous = prev_frame(roi).clone();                // Create Masked frame for motion detect  Masked Size BW
-
+            masked_frame_previous = prev_frame(roi).clone(); // Create Masked frame for motion detect  Masked Size BW
         }
         else
         {
@@ -121,8 +115,7 @@ bool get_camera_frame(cv::VideoCapture &capture, cv::Mat &gray_frame_local, cv::
             if (!prev_frame.empty())
             {
                 // Create smaller image for detecting motion in the central 75%
-                masked_frame_current = gray_frame_local(roi).clone(); 
-
+                masked_frame_current = gray_frame_local(roi).clone();
 
                 // caclulate the absoute difference of the previousframe and current frame
                 cv::absdiff(masked_frame_current, masked_frame_previous, diff_frame);
@@ -131,80 +124,26 @@ bool get_camera_frame(cv::VideoCapture &capture, cv::Mat &gray_frame_local, cv::
                 // diff_frame *= 10;
 
                 // sum the number of pixels that have moved and divide by 255
-                cv::Scalar sum_diff = cv::sum(diff_frame) ;
+                sum_diff = cv::sum(diff_frame);
 
                 nonZeroCount = cv::countNonZero(diff_frame);
 
-                if( nonZeroCount > Motion_Thresh)
+                if (nonZeroCount > Motion_Thresh)
                     Image_Motion = true;
-
-
-
-                // timing measure
-                auto loopEndTime = std::chrono::steady_clock::now();
-                std::chrono::duration<double> elapsed_seconds = loopEndTime - loopStartTime;
-                std::cout << "SUMMMMMMMMMMMMMMMMMMMMMMMMMMM " << elapsed_seconds.count() << "  " << sum_diff << "   nonZeroCount " << nonZeroCount  << "\n" ;
-                // std::cout << " " << Image_Motion << "\n" ;
             }
         }
-        full_cycle = !full_cycle ;
+
+        // timing measure
+        auto loopEndTime = std::chrono::steady_clock::now();
+        std::chrono::duration<double> elapsed_seconds = loopEndTime - loopStartTime;
+        std::cout << "camera stuff " << full_cycle << "  " << elapsed_seconds.count() << "  " << sum_diff << "   nonZeroCount " << nonZeroCount << "\n";
+        // std::cout << " " << Image_Motion << "\n" ;
+
+        full_cycle = !full_cycle;
     }
 
     // if(Image_Motion)
     //     std::cout << "Image_Motion " << Image_Motion << "nonZeroCount " << nonZeroCount <<  std::endl;
 
     return Image_Motion;
-
 }
-
-
-// void get_camera_frame_Test(cv::VideoCapture &capture, cv::Mat &gray_frame_local) // bool init, cv::Mat gray_frame)
-// {
-//     static int loop_count = 418;
-
-//     static auto Sample_Time_Stored = std::chrono::steady_clock::now();
-//     static auto Sample_Time_Current = std::chrono::steady_clock::now();    
-//     static std::chrono::duration<double> Cycle_Time = Sample_Time_Current - Sample_Time_Stored;
-//     static bool full_cycle = false;
-
-//     static cv::Mat frame_local; // (768, 1024, CV_8UC1); 
-//     static cv::Mat prev_frame; // (768, 1024, CV_8UC1);
-
-//     static cv::Mat masked_frame_previous(Motion_Window_V_Size, Motion_Window_H_Size, CV_8UC1);
-//     static cv::Mat masked_frame_current(Motion_Window_V_Size, Motion_Window_H_Size, CV_8UC1);
-
-//     cv::Rect roi(Motion_Window_H_Pos, Motion_Window_V_Pos, Motion_Window_H_Size, Motion_Window_V_Size);
-
-//     loop_count++;
-//     // std::cout << "loop_cnt " << loop_count << std::endl;
-
-//     if (loop_count >= 440)
-//         loop_count = 0;
-
-
-//     Sample_Time_Current = std::chrono::steady_clock::now();  
-//     Cycle_Time = Sample_Time_Current - Sample_Time_Stored;
-
-//     if(Cycle_Time.count() >= Half_Frame_Duration)
-//     {
-//         Sample_Time_Stored = Sample_Time_Current;   
-
-//        auto loopStartTime = std::chrono::steady_clock::now();
-
-//         capture >> frame_local;                     // // Camera Size Color
-//         // If the frame is empty, break the loop
-//         if (frame_local.empty())
-//         {
-//             printf("Error: Could not capture frame XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX.\n");
-//             // break;
-//         }
-//             // Convert to BW   (Camera Size BW)
-//             cv::cvtColor(frame_local, gray_frame_local, cv::COLOR_BGR2GRAY);      // Convert to BW   Camera Size BW
-
-//             // Resize the grayscale camera frame to Screen Size BW
-//             cv::resize(gray_frame_local, gray_frame_local, cv::Size(Screen_H_Size, Screen_V_Size));      
-
-//         full_cycle = !full_cycle ;
-//     }
-// }
-
--- main.cpp
+++ main.cpp
@@ -1,11 +1,25 @@
 #include <iostream>
-#include <opencv2/opencv.hpp>
+#include <fstream>
+#include <sstream>
 #include <chrono>
-#include "camera_grab.h"
-
+#include <ctime>
 #include <sys/select.h>
 #include <unistd.h>
-#include <ctime>
+#include <filesystem>
+#include <regex>
+#include <thread>
+
+#include <opencv2/opencv.hpp>
+
+#include "camera_grab.h"
+
+
+
+// #include <boost/filesystem.hpp>
+
+
+// namespace fs = boost::filesystem;
+
 
 // #define Cam_H_Size 800
 // #define Cam_V_Size 600
@@ -15,6 +29,8 @@
 
 // #define Motion_Window_H_Size Screen_H_Size * .75
 // #define Motion_Window_V_Size Screen_V_Size * .75
+
+namespace fs = std::filesystem;
 
 struct Client_Parameters_Main
 {
@@ -31,7 +47,6 @@ struct Client_Parameters_Main
     int Noise_Threshold;
     int Motion_Threshold;
 
-
     std::string param3;
     // std::string param4;
 
@@ -43,13 +58,9 @@ struct Client_Parameters_Main
     int Motion_Window_H_Size = (Screen_H_Size * Motion_Window_H_Size_Multiplier) / 100;
     int Motion_Window_V_Size = (Screen_V_Size * Motion_Window_V_Size_Multiplier) / 100;
 
-    int Motion_Window_H_Position = (Screen_H_Size -  Motion_Window_H_Size ) / 2;
-    int Motion_Window_V_Position = (Screen_V_Size -  Motion_Window_V_Size ) / 2;
+    int Motion_Window_H_Position = (Screen_H_Size - Motion_Window_H_Size) / 2;
+    int Motion_Window_V_Position = (Screen_V_Size - Motion_Window_V_Size) / 2;
 };
-
-
-
-
 
 void readParametersFromFile(const std::string &filename, Client_Parameters_Main &params)
 {
@@ -115,46 +126,100 @@ void readParametersFromFile(const std::string &filename, Client_Parameters_Main 
         }
         else if (name == "String_Test")
         {
-            params.param3 = value;  // String value
+            params.param3 = value; // String value
         }
 
         params.Motion_Window_H_Size = (params.Screen_H_Size * params.Motion_Window_H_Size_Multiplier) / 100;
         params.Motion_Window_V_Size = (params.Screen_V_Size * params.Motion_Window_V_Size_Multiplier) / 100;
 
-        params.Motion_Window_H_Position = (params.Screen_H_Size -  params.Motion_Window_H_Size ) / 2;
-        params.Motion_Window_V_Position = (params.Screen_V_Size -  params.Motion_Window_V_Size ) / 2;
-
+        params.Motion_Window_H_Position = (params.Screen_H_Size - params.Motion_Window_H_Size) / 2;
+        params.Motion_Window_V_Position = (params.Screen_V_Size - params.Motion_Window_V_Size) / 2;
     }
 }
+
+std::string getNextFileName(const std::string& directory) {
+    std::regex filePattern(R"((\d+)\.raw)");
+
+    int maxNumber = -1;
+
+    // Iterate through the files in the directory
+    for (const auto& entry : fs::directory_iterator(directory)) {
+        if (fs::is_regular_file(entry)) {
+            std::string filename = entry.path().filename().string();
+            std::smatch match;
+            if (std::regex_match(filename, match, filePattern)) {
+                int number = std::stoi(match[1]);
+                if (number > maxNumber) {
+                    maxNumber = number;
+                }
+            }
+        }
+    }
+
+    // Determine the new file name
+    int newFileNumber = maxNumber + 1;
+    std::string newFilename = std::to_string(newFileNumber).insert(0, 6 - std::to_string(newFileNumber).length(), '0') + ".raw";
+
+    return directory + newFilename;
+}
+
+
+
+void writeMatRawData(const cv::Mat &mat, const std::string &filename)
+{
+    // Open a binary file for writing
+    std::ofstream outFile(filename, std::ios::binary);
+
+    // Check if the file is opened successfully
+    if (!outFile)
+    {
+        std::cerr << "Error opening file for writing!" << std::endl;
+        return;
+    }
+
+    // Write matrix data (no additional metadata)
+    outFile.write(reinterpret_cast<const char *>(mat.data), mat.total() * mat.elemSize());
+
+    // Close the file
+    outFile.close();
+}
+
+
 
 
 #define Minimum_Time_Between_Storing_Images 1
 
-void Sequencer(const bool Image_Motion, const cv::Mat &gray_frame_local )
+void Sequencer(const bool Image_Motion, const cv::Mat &gray_frame_local)
 {
     static std::time_t currentTime = std::time(nullptr);
-    static std::tm* localTime = std::localtime(&currentTime);
+    static std::tm *localTime = std::localtime(&currentTime);
     static unsigned long last_image_stored = 0;
     static unsigned long time_since_last_iage_stored = 0;
 
-
-    // std::cout << "Current time in 24-hour format: " 
-    //           << std::put_time(localTime, "%H:%M:%S") 
+    // std::cout << "Current time in 24-hour format: "
+    //           << std::put_time(localTime, "%H:%M:%S")
     //           << " "  << currentTime <<  std::endl;
-
 
     currentTime = std::time(nullptr);
     time_since_last_iage_stored = currentTime - last_image_stored;
-    if( (time_since_last_iage_stored > 5) && Image_Motion)
+    if ((time_since_last_iage_stored > 10) && Image_Motion)
     {
         last_image_stored = currentTime;
-        std::cout << " NOWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW " << std::endl;
+
+
+        auto loopStartTime = std::chrono::steady_clock::now();
+
+        std::string nextFileName = getNextFileName("./raw/");
+        writeMatRawData(gray_frame_local, nextFileName);
+
+        auto loopEndTime = std::chrono::steady_clock::now();
+
+        std::chrono::duration<double> elapsed_seconds = loopEndTime - loopStartTime;
+        // std::cout << "Loop duration: " << elapsed_seconds.count() << "s\n";
+
+        std::cout << "WRITING next filename " << nextFileName <<  "  time  "  << elapsed_seconds.count() << std::endl;
     }
-
 }
-
-
-
 
 int main()
 {
@@ -164,29 +229,27 @@ int main()
     bool displayImage = true;
     bool displayMotion = true;
     bool Image_Motion = false;
+    bool Image_Motion_Delayed = false;
+    long loopCount=0;
 
     Client_Parameters_Main Client_Params;
 
-
     readParametersFromFile("memory_params2.txt", Client_Params);
-
-
 
     std::cout << "Parameter 1: " << Client_Params.Cam_H_Size << std::endl;
     std::cout << "Parameter 2: " << Client_Params.Cam_V_Size << std::endl;
     std::cout << "Parameter 3: " << Client_Params.Screen_H_Size << std::endl;
     std::cout << "Parameter 4: " << Client_Params.Screen_V_Size << std::endl;
     std::cout << "Parameter 5: " << Client_Params.Motion_Window_H_Size << std::endl;
-    std::cout << "Parameter 6: " << Client_Params.Motion_Window_V_Size << std::endl;    
+    std::cout << "Parameter 6: " << Client_Params.Motion_Window_V_Size << std::endl;
     std::cout << "Parameter 7: " << Client_Params.Motion_Window_H_Position << std::endl;
-    std::cout << "Parameter 8: " << Client_Params.Motion_Window_V_Position << std::endl;      
-    std::cout << "Parameter 8: " << Client_Params.Cycle_Time << std::endl;         
-    std::cout << "Parameter 8: " << Client_Params.param3 << std::endl;             
+    std::cout << "Parameter 8: " << Client_Params.Motion_Window_V_Position << std::endl;
+    std::cout << "Parameter 8: " << Client_Params.Cycle_Time << std::endl;
+    std::cout << "Parameter 8: " << Client_Params.param3 << std::endl;
 
     // Sequencer();
 
     // exit(0);
-
 
     std::string imageFile = "../../images/image.jpg"; // Path to your image file
 
@@ -199,15 +262,14 @@ int main()
 
     cv::namedWindow("Test Webcam Feed", cv::WINDOW_AUTOSIZE);
 
+    std::cout << " HERR " << getNextFileName("./raw/") << std::endl;
+
     while (true)
     {
 
-
         // get camera frame and noise
-        Image_Motion = get_camera_frame(cap, gray_frame, frame_Abs_Diff, Client_Params.Cycle_Time,  Client_Params.Motion_Window_H_Position, Client_Params.Motion_Window_V_Position, Client_Params.Noise_Threshold,Client_Params.Motion_Threshold )    ;
-
-
-
+        Image_Motion_Delayed = Image_Motion;
+        Image_Motion = get_camera_frame(cap, gray_frame, frame_Abs_Diff, Client_Params.Cycle_Time, Client_Params.Motion_Window_H_Position, Client_Params.Motion_Window_V_Position, Client_Params.Noise_Threshold, Client_Params.Motion_Threshold);
 
         //  get_camera_frame_Test(cap, gray_frame);
 
@@ -250,19 +312,26 @@ int main()
         }
 
 
-        Sequencer(Image_Motion, gray_frame);
+
+        Sequencer(Image_Motion_Delayed, gray_frame);
 
         // if(Image_Motion)
         //   std::cout << "MAIN  Image_Motion " << Image_Motion <<   std::endl;
 
-
         // timing
+
+        // std::thread worker(fileHandlingThread, std::cref(gray_frame), directory);
+
         auto loopEndTime = std::chrono::steady_clock::now();
         std::chrono::duration<double> elapsed_seconds = loopEndTime - loopStartTime;
         // std::cout << "Loop duration: " << elapsed_seconds.count() << "s\n";
         if (elapsed_seconds.count() > .03333)
             std::cout << "Loop duration BIGGGGGGGGGGGGGGGGGGGGGGG: " << elapsed_seconds.count() << "s\n";
+        
+        if(Image_Motion || Image_Motion_Delayed)
+            std::cout << "Loop duration XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXxxx: " << elapsed_seconds.count() << "s\n";            
         loopStartTime = std::chrono::steady_clock::now();
+        loopCount++;
     }
 
     return 0;
